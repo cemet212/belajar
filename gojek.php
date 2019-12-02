fe8839daac799bbc2bc6b1485149e8b83f638a6e
cemet212 <57763344+cemet212@users.noreply.github.com>
2019-12-02 14:02:23 +0700

    Add files via upload

--- gojek.php
+++ gojek.php
@@ -9,46 +9,46 @@ include ("curl.php");
 echo "\n";
 echo "\e[94m          BERBURU PROMO MEDAN           \n";
 echo "\e[91m FORMAT NOMOR HP : INDONESIA '62***' , US='1***'\n";
-echo "\e[93m JANGAN KELEN SEBAR INI YA !!! HARGAI\n";
-echo "\e[93m DZULYUSRI\n";
-echo "\e[93m CYBER GOJEK FREE \n";
+echo "\e[93m HANYA UNTUK REGISTER DAN CLAIM VOUCHER\n";
+echo "\e[93m CREATE BY ZY FLASHER MEDAN\n";
+echo "\e[93m MAKAN GAK HARUS MAHAL \n";
 echo "\n";
-echo "\e[96m[?] Masukkan Nomor HP KAU BUJANG (62/1) : ";
+echo "\e[96m[X] Masukkan Nomor HP Anda (62/1) : ";
 $nope = trim(fgets(STDIN));
 $register = register($nope);
 if ($register == false)
     {
-    echo "\e[91m[x] Nomer Bekas Kau Masukan\n";
+    echo "\e[91m[X] Nomer Bekas Kau Masukan\n";
     }
   else
     {
     otp:
-    echo "\e[96m[!] Mana (OTP) Kau : ";
+    echo "\e[96m[X] Masukan OTP Bro : ";
     $otp = trim(fgets(STDIN));
     $verif = verif($otp, $register);
     if ($verif == false)
         {
-        echo "\e[91m[x] Salah Itu Ko Lihat\n";
+        echo "\e[91m[X] Kode OTP Salah\n";
         goto otp;
         }
       else
         {
         file_put_contents("token/".$verif['data']['customer']['name'].".txt", $verif['data']['access_token']);
-        echo "\e[93m[!] Trying to redeem Voucher : GOFOODSANTAI30K !\n";
+        echo "\e[93m[X] Mencoba Redeem : GOFOOD 30K !\n";
         sleep(3);
         $claim = claim($verif);
         if ($claim == false)
             {
             echo "\e[92m[!]".$voucher."\n";
             sleep(3);
-            echo "\e[93m[!] Coba Dulu Redeem : GOFOODSANTAI25K !\n";
+            echo "\e[93m[X] Mencoba Redeem : GOFOOD 25K !\n";
             sleep(3);
             goto next;
             }
             else{
                 echo "\e[92m[+] ".$claim."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : COBAINGOJEK !\n";
+                echo "\e[93m[X] Mencoba Redeem : WADAW !\n";
                 sleep(3);
                 goto ride;
             }
@@ -57,14 +57,14 @@ if ($register == false)
             if ($claim == false) {
                 echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : GOFOODSANTAI20K !\n";
+                echo "\e[93m[X] Mencoba Redeem : GOFOOD 20K !\n";
                 sleep(3);
                 goto next1;
             }
             else{
                 echo "\e[92m[+] ".$claim."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : COBAINGOJEK !\n";
+                echo "\e[93m[X] Mencoba Redeem : WADAW !\n";
                 sleep(3);
                 goto ride;
             }
@@ -73,7 +73,7 @@ if ($register == false)
             if ($claim == false) {
                 echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : COBAINGOJEK !\n";
+                echo "\e[93m[X] Mencoba Redeem : WADAW !\n";
                 sleep(3);
                 goto ride;
             }
@@ -81,30 +81,31 @@ if ($register == false)
             {
             echo "\e[92m[+] ".$claim . "\n";
             sleep(3);
-            echo "\e[93m[!] Coba Dulu Redeem : COBAINGOJEK !\n";
+            echo "\e[93m[X] Mencoba Redeem : WADAW !\n";
             sleep(3);
             goto ride;
             }
             ride:
             $claim = ride($verif);
-            if ($claim == false ) {
+            if ($claim == false ) 
+           {
                 echo "\e[92m[!]".$claim['errors'][0]['message']."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : AYOCOBAGOJEK !\n";
+                echo "\e[93m[X] Mencoba Redeem : AYOCOBAGOJEK !\n";
                 sleep(3);
 
             }
             else{
                 echo "\e[92m[+] ".$claim."\n";
                 sleep(3);
-                echo "\e[93m[!] Coba Dulu Redeem : AYOCOBAGOJEK !\n";
+                echo "\e[93m[X] Mencoba Redeem : AYOCOBAGOJEK !\n";
                 sleep(3);
                 goto pengen;
             }
             pengen:
             $claim = cekvocer($verif);
             if ($claim == false ) {
-                echo "\033VOUCHER INVALID/GAGAL REDEEM\n";
+                echo "Maaf Akun Anda Tidak Ada Voucher\n";
             }
             else{
                 echo "\e[92m[+] ".$claim."\n";
